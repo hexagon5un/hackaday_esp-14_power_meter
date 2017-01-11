323b9a998c62965fb82c1a2c6c8be0d1b01b7ac0
Elliot Williams <elliot@pre.postero.us>
2017-01-11 22:17:18 +0100

    pruning exploratory dead ends

--- power_meter.fs
+++ power_meter.fs
@@ -4,28 +4,6 @@ nvm
 : chan.a!   2 adc! ;
 : chan.b!   3 adc! ;
 
-\ timer ticks are approx 5 ms
-\ : tick tim begin dup tim 31 and = until ;
-
-\ : amax ( times  -- max ADC ) 0 swap 0 do chan.a adc  max loop ;
-\ : amin ( times  -- min ADC ) 1000 swap 0 do chan.a adc  min loop ;
-\ : arange ( times -- range ) dup amax swap amin - ;
-\ : bmax ( times  -- max ADC ) 0 swap 0 do chan.b adc  max loop ;
-\ : bmin ( times  -- min ADC ) 1000 swap 0 do chan.b adc  min loop ;
-\ : brange ( times -- range ) dup bmax swap bmin - ;
-
-\ seems to be working, needs delay -- reading too fast.  was using dup and print, which works, but wastes juice
-
-\ : f pa3.on 500 10 0 do ewma loop . pa3.off ;
-\ : tt pa3.on adc@ 15 0 do adc@ + loop 16 / pa3.off ; 
-\ : 3ewma ewma dup ewma dup ewma ;
-\ : 3emit rot . swap . dup . cr ;
-\ : foo adc@ begin pa3.toggle 3ewma pa3.toggle 3emit ?key until ;
-
-\ what if don't set the channel?
-\ : minmax adc@ min swap adc@ max swap ;
-\ : ff pa3.on 0 1024 1200 0 do minmax loop pa3.off - . ;
-
 : 5ms tim 1+ begin dup tim = until drop ;
 : average 0 10 0 do adc@ + 5ms loop 10 /  ;
 ram
