70250aff5f12d7a696f8b7b708164584f75bd2d6
Elliot Williams <elliot@pre.postero.us>
2017-01-13 13:37:59 +0100

    it lives!
    I'm liking this flow of putting the data on the stack
    and then just shooting it off
    Stack seems to be good for 80 bytes or so.
    Need to check.

--- mqtt.fs
+++ mqtt.fs
@@ -2,23 +2,27 @@
 
 hex
 nvm
-: emits 0 do emit loop ;  
+
+: emits 1 + dup 0 
+  do dup i - 2 * sp@ + @ emit loop drop ;  
+  \ reverses stack, emits
+: cleanup 0 do drop loop ;
 
 : sync c0 
 	01 00  00 00  89 02  00 00 
 	0a e0 
-	c0 0c emits 
+	c0 \ 0c emits 0c cleanup
 	;
 
-: mqtt-setup  c0 
-	0a 00  04 00  00 00  00 00 
-	04 00  6b 02  00 00  \ length, bytes
-	04 00  71 02  00 00  
-	04 00  77 02  00 00  
-	04 00  7d 02  00 00 
-	dc 43 
-	c0 24 emits 
-	;
+\ : mqtt-setup  c0 
+	\ 0a 00  04 00  00 00  00 00 
+	\ 04 00  6b 02  00 00  \ length, bytes
+	\ 04 00  71 02  00 00  
+	\ 04 00  77 02  00 00  
+	\ 04 00  7d 02  00 00 
+	\ dc 43 
+	\ c0 \ 24 emits 24 cleanup
+	\ ;
 
 : message0 c0 
 	0b 00  05 00  00 00  00 00 
@@ -29,14 +33,15 @@ nvm
 	01 00  00 00  00 00  \ qos
 	01 00  00 00  00 00  \ retain
 	e5 ae 
-	c0 32 emits
+	c0 \ 32 emits 32 cleanup
 	;
 
 : get-time c0 
 	07 00  00 00  00 00  00 00  
 	0e 9c  
-	c0 0c emits
+	c0 \ 0c emits 0c cleanup
 ;
+
 ram
 
 dec
